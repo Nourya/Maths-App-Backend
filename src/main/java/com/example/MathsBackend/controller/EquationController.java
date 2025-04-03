0948eb2699b1edd2723d933c278fa4b1ddd0cc0e
Nourya <nouryachihi@gmail.com>
2025-04-04 01:57:10 +0200

    Allow Angular

--- src/main/java/com/example/MathsBackend/controller/EquationController.java
+++ src/main/java/com/example/MathsBackend/controller/EquationController.java
@@ -3,13 +3,11 @@ package com.example.MathsBackend.controller;
 import com.example.MathsBackend.model.EquationRequest;
 import com.example.MathsBackend.model.EquationResponse;
 import com.example.MathsBackend.service.EquationService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/equation")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EquationController {
     private final EquationService equationService;
     public EquationController (EquationService equationService){
